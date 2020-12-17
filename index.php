8959669b5f7dec7a781d85bf9232c7c58e3e6c18
Massimiliano Ambrogio <maxi.ambrogio@gmail.com>
2020-12-17 18:00:29 +0100

    Project 44 parte 1.0 add and fix html php scss js

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+// Dati albums 
+include __DIR__ . '/partial/database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax Dischi</title>
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="dist/css/app.css">
+</head>
+<body>
+    <!-- Inizio esercizio php ajax dischi -->
+
+    <div id="app">
+        <!-- Header-top -->
+        <header>
+            <div class="cont-logo">
+                <img src="./img/logo.png" alt="Spotify-logo">
+            </div>
+        </header>
+
+        <!-- Main-content -->
+        <main>
+            <section class="database">
+                <div class="container">
+                    <?php foreach ($database as $data) { ?>
+                        <img src="<?php echo $data['poster'] ?>" alt="Img Album">
+                        <h2> <?php echo $data['title'] ?></h2>
+                        <p> <?php echo $data['author'] ?></p>
+                        <p> <?php echo $data['year'] ?></p>
+                        <p> <?php echo $data['genre'] ?></p>
+                    <?php } ?>
+                </div>
+            </section>
+        </main>
+    </div>
+
+    <!-- JS  -->
+    <script src="./dist/js/main.js"></script>
+
+    <!-- Inizio esercizio php ajax dischi -->
+</body>
+</html>
